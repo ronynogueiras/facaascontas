8744016635173d9836ce3b08f3c67bb386fde759
Rony Nogueira <ronynogueiras.dev@gmail.com>
2017-10-30 02:13:56 -0200

    adição de models para vencimento e salvamento de despesa

--- src/components/ManagerExpensePage.vue
+++ src/components/ManagerExpensePage.vue
@@ -1,6 +1,6 @@
 <template>
   <v-ons-page id="manager-expense">
-    <v-ons-list>
+    <v-ons-list nodivider>
       <v-ons-list-header>
         <div class="center">
           Despesa
@@ -16,27 +16,38 @@
         <label for="">Vencimento</label>
         <v-ons-row>
           <v-ons-col width="20%">
-            <v-ons-select style="width: 100%">
+            <v-ons-select style="width: 100%" v-model="day">
               <option v-for="day in 31" :value="day" :key="day">
                 {{ day }}
               </option>
             </v-ons-select>
           </v-ons-col>
           <v-ons-col  width="60%">
-            <v-ons-select style="width: 100%">
+            <v-ons-select style="width: 100%" v-model="month">
               <option v-for="month in months" :value="month.id" :key="month.id">
                 {{ month.name }}
               </option>
             </v-ons-select>
           </v-ons-col>
           <v-ons-col  width="20%">
-            <v-ons-select style="width: 100%">
+            <v-ons-select style="width: 100%" v-model="year">
             <option v-for="year in years" :value="year" :key="year">
               {{ year }}
             </option>
           </v-ons-select>
           </v-ons-col>
         </v-ons-row>
+      </v-ons-list-item>
+      <v-ons-list-item>
+        <v-ons-input type="tel" placeholder="Parcelas" float v-model="parcels"></v-ons-input>
+      </v-ons-list-item>
+      <v-ons-list-item>
+        <label class="left">
+          <v-ons-checkbox :input-id="'paid'" v-model="paid"/>
+        </label>
+        <label class="center" :for="'paid'">
+          Pago
+        </label>
       </v-ons-list-item>
     </v-ons-list>
     <v-ons-bottom-toolbar>
@@ -50,11 +61,21 @@ import { VMoney } from 'v-money';
 
 export default {
   name: 'manager-expense',
+  props: {
+    id: {
+      type: Number,
+      default: 0,
+    },
+  },
   data() {
     return {
       description: '',
-      dueDate: '',
+      day: new Date().getDate(),
+      month: new Date().getMonth() + 1,
+      year: new Date().getFullYear(),
       value: 0.0,
+      paid: false,
+      parcels: 1,
       months: [
         {
           id: 1,
@@ -108,7 +129,7 @@ export default {
       money: {
         decimal: ',',
         thousands: '.',
-        prefix: 'R$ ',
+        prefix: 'R$',
         suffix: '',
         precision: 2,
         masked: false,
@@ -117,11 +138,30 @@ export default {
   },
   methods: {
     save() {
-      alert(JSON.stringify({
+      const expense = {
         description: this.description,
-        dueDate: this.dueDate,
-        value: this.value,
-      }));
+        paid: this.paid,
+        due_date: this.getDueDate(),
+        parcels: this.parcels ? this.parcels : 1,
+        value: this.value.replace(/[^0-9,]/g, '').replace(',', '.'),
+      };
+      this.$db.expenses.put(expense).then(() => {
+        this.description = '';
+        this.paid = false;
+        this.day = new Date().getDate();
+        this.month = new Date().getMonth() + 1;
+        this.year = new Date().getFullYear();
+        this.value = 0.0;
+        this.$ons.notification.toast('Salvo com sucesso.', { timeout: 1200 });
+      }).catch((err) => {
+        console.log(err);
+        this.$ons.notification.toast('Falha ao salvar', { timeout: 1200 });
+      });
+    },
+    getDueDate() {
+      const day = this.day < 10 ? `0${this.day}` : this.day;
+      const month = this.month < 10 ? `0${this.month}` : this.month;
+      return `${this.year}-${month}-${day}`;
     },
   },
   computed: {
