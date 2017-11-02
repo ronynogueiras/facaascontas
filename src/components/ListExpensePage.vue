0e70e254ae2f9389bb74edc6f683b32102641d0d
Rony Nogueira <ronynogueiras.dev@gmail.com>
2017-11-02 15:54:24 -0200

    adição de crud para despesas de um mês

--- src/components/ListExpensePage.vue
+++ src/components/ListExpensePage.vue
@@ -1,17 +1,17 @@
 <template>
   <v-ons-page id="list-expense">
     <v-ons-row>
-      <v-ons-col>
-        <v-ons-button modifier="outline">
-          <v-ons-icon icon="md-arrow-left"></v-ons-icon>
+      <v-ons-col width="20%">
+        <v-ons-button modifier="outline" @click.prevent="prevMonth()">
+          <v-ons-icon icon="ion-arrow-left-c"></v-ons-icon>
         </v-ons-button>
       </v-ons-col>
-      <v-ons-col>
-        <div class="text-center title">Outubro 2017</div>
+      <v-ons-col width="60%">
+        <div class="text-center title">{{ currentMonth }}</div>
       </v-ons-col>
-      <v-ons-col class="text-right">
-        <v-ons-button modifier="outline">
-          <v-ons-icon icon="md-arrow-right"></v-ons-icon>
+      <v-ons-col width="20%" class="text-right">
+        <v-ons-button modifier="outline" @click.prevent="nextMonth()">
+          <v-ons-icon icon="ion-arrow-right-c"></v-ons-icon>
         </v-ons-button>
       </v-ons-col>
     </v-ons-row>
@@ -23,14 +23,17 @@
           <v-ons-col  class="text-right"><b>Valor</b></v-ons-col>
         </v-ons-row>
       </v-ons-list-item>
-      <v-ons-list-item v-for="(item, index) in expenses" :key="item.id" tappable @click.prevent="showDialogOptions(index)">
+      <v-ons-list-item v-for="(item, index) in expenses" :key="item.id" tappable @click.prevent="showDialogOptions(index)" v-if="expenses.length > 0">
         <v-ons-row>
           <v-ons-col width="10%"><v-ons-icon :icon="item.paid ? 'ion-checkmark-round' : 'ion-close-round'"></v-ons-icon></v-ons-col>
-          <v-ons-col width="60%">{{ item.description }}<br><small>{{ item.due_date }}</small></v-ons-col>
+          <v-ons-col width="60%">{{ item.description }}<br><small>{{ parseDueDate(item.due_date) }}</small></v-ons-col>
           <v-ons-col class="text-right" vertical-align="center">
               {{ item.value | money }}
           </v-ons-col>
         </v-ons-row>
+      </v-ons-list-item>
+      <v-ons-list-item v-if="expenses.length === 0">
+        Nenhuma despesa neste mês
       </v-ons-list-item>
     </v-ons-list>
     <v-ons-alert-dialog modifier="rowfooter" :visible.sync="dialogVisible">
@@ -49,7 +52,7 @@
             <v-ons-icon :icon="iconPaid" class="list-item__icon"></v-ons-icon>
           </div>
           <div class="center">
-            {{ expenses[current] && expenses[current].paid ? 'Pago' : 'Não Pago' }}
+            {{ expenses[current] && expenses[current].paid ? 'Marcar como Não Pago' : 'Marcar como Pago' }}
           </div>
         </v-ons-list-item>
         <v-ons-list-item @click="showRemoveAlert()" tappable>
@@ -75,37 +78,28 @@
 </template>
 
 <script>
+import monthsName from '@/util/months';
+
 export default {
   name: 'list-expense',
   props: {
     month: {
       type: String,
     },
-    year: null,
   },
   data() {
     return {
       dialogVisible: false,
       current: null,
-      expenses: [
-        {
-          id: 1,
-          value: 100.11,
-          paid: false,
-          due_date: '30/10/2017',
-          description: 'Despesa',
-        },
-        {
-          id: 2,
-          value: 102.12,
-          paid: true,
-          due_date: '31/10/2017',
-          description: 'Despesa',
-        },
-      ],
+      expenses: [],
     };
   },
   methods: {
+    get() {
+      this.$db.expenses.where('due_date').startsWith(this.month).toArray((arr) => {
+        this.expenses = arr;
+      });
+    },
     showDialogOptions(index) {
       this.current = index;
       this.dialogVisible = true;
@@ -120,23 +114,62 @@ export default {
         buttonLabels: ['Não', 'Sim'],
       }).then((result) => {
         if (result === 1) {
-          this.hideDialogOptions();
           this.removeItem();
         }
       });
     },
     togglePayment() {
       this.expenses[this.current].paid = !this.expenses[this.current].paid;
-      this.$ons.notification.toast(this.expenses[this.current].paid ? 'Marcado como pago' : 'Marcado como não pago', {
-        timeout: 1200,
+      this.$db.expenses.update(this.expenses[this.current].id, {
+        paid: this.expenses[this.current].paid,
+      }).then(() => {
+        const message = this.expenses[this.current].paid ? 'Marcado como pago' : 'Marcado como não pago';
+        this.$ons.notification.toast(message, {
+          timeout: 1200,
+        });
+        this.hideDialogOptions();
       });
-      this.hideDialogOptions();
     },
     removeItem() {
-      this.$ons.notification.toast('Removido!', {
-        timeout: 1200,
+      const id = this.expenses[this.current].id;
+      this.$db.expenses.delete(id).then((result) => {
+        const message = result ? 'Removido com sucesso.' : 'Falha ao remover';
+        this.$ons.notification.toast(message, {
+          timeout: 1200,
+        });
+        this.hideDialogOptions();
+        this.get();
       });
     },
+    parseDueDate(date) {
+      const aux = date.split('-');
+      return `${aux[2]}/${aux[1]}/${aux[0]}`;
+    },
+    nextMonth() {
+      const currentMonth = parseInt(this.month.split('-')[1], 10) + 1;
+      const currentYear = parseInt(this.month.split('-')[0], 10);
+      let next;
+      if (currentMonth > 12) {
+        next = { name: 'list_expenses', params: { month: `${currentYear + 1}-01` } };
+      } else {
+        next = { name: 'list_expenses', params: { month: `${currentYear}-${currentMonth < 10 ? '0' : ''}${currentMonth}` } };
+      }
+      this.$router.push(next);
+    },
+    prevMonth() {
+      const currentMonth = parseInt(this.month.split('-')[1], 10) - 1;
+      const currentYear = parseInt(this.month.split('-')[0], 10);
+      let prev;
+      if (currentMonth === 0) {
+        prev = { name: 'list_expenses', params: { month: `${currentYear - 1}-12` } };
+      } else {
+        prev = { name: 'list_expenses', params: { month: `${currentYear}-${currentMonth < 10 ? '0' : ''}${currentMonth}` } };
+      }
+      this.$router.push(prev);
+    },
+  },
+  watch: {
+    $route: 'get',
   },
   computed: {
     total() {
@@ -148,6 +181,13 @@ export default {
       'ion-checkmark-round' :
       'ion-close-round';
     },
+    currentMonth() {
+      const date = this.month.split('-');
+      return `${monthsName[date[1] - 1].name} de ${date[0]}`;
+    },
+  },
+  mounted() {
+    this.get();
   },
 };
 </script>
