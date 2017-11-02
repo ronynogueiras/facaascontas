e56d9532a0a88b4451bf6b171ade62988ccf74d2
Rony Nogueira <ronynogueiras.dev@gmail.com>
2017-11-02 15:53:49 -0200

    adição de listagem de próximos meses

--- src/components/ListMonthsExpensesPage.vue
+++ src/components/ListMonthsExpensesPage.vue
@@ -6,19 +6,64 @@
           Meses
         </div>
       </v-ons-list-header>
-      <v-ons-list-item tappable v-for="item in 12" :key="item" @click.prevent="$router.push({name: 'list_expenses', params: { month: '10', year: 2017}})" modifier="chevron">
+      <v-ons-list-header>
         <v-ons-row>
-          <v-ons-col width="60%">Outubro 2017</v-ons-col>
-          <v-ons-col>R$100,00</v-ons-col>
+          <v-ons-col width="60%">Mês</v-ons-col>
+          <v-ons-col>Valor Total </v-ons-col>
+        </v-ons-row>
+      </v-ons-list-header>
+      <v-ons-list-item tappable v-for="(month, index) in months" :key="index" @click.prevent="$router.push({name: 'list_expenses', params: { month: index}})" modifier="chevron" v-if="months != {}">
+        <v-ons-row>
+          <v-ons-col width="60%">{{ monthsName[index.substr(5, 2) - 1 ] }}, {{ index.substr(0, 4) }}</v-ons-col>
+          <v-ons-col>{{ total(month) | money }} </v-ons-col>
         </v-ons-row>
       </v-ons-list-item>
+      <v-ons-list-item v-else>
+        <div class="center">
+          Nenhuma despesa cadastrada.
+        </div>
+      </v-ons-list-item>      
     </v-ons-list>
   </v-ons-page>
 </template>
 
 <script>
+import _ from 'lodash';
+import monthsName from '@/util/months';
+
 export default {
   name: 'list-months-expenses',
+  data() {
+    return {
+      months: [],
+    };
+  },
+  computed: {
+    monthsName() {
+      return _.map(monthsName, item => item.name);
+    },
+  },
+  methods: {
+    get() {
+      this.$db.expenses.orderBy('id').toArray((arr) => {
+        if (arr.length > 0) {
+          let months = _.map(arr, (o) => {
+            const i = o;
+            i.due_date = o.due_date.split('-').splice(0, 2).join('-');
+            return i;
+          });
+          months = _.groupBy(months, o => o.due_date);
+          this.months = months;
+        }
+      });
+    },
+    total(month) {
+      return month.reduce((total, current) => total + parseFloat(current.value), 0);
+    },
+  },
+  mounted() {
+    this.get();
+  },
 };
 </script>
 
